660ff05a7c0c6e383b786781dd1726935dec42d1
HenrikKorhonen <hevemiko@student.jyu.fi>
2016-11-27 05:19:10 +0200

    better now

--- messages/index.js
+++ messages/index.js
@@ -29,9 +29,83 @@ bot.on('trigger', function (message) {
         .text('This is coming from the trigger: ' + queuedMessage.text);
     bot.send(reply);
 });
+var luis = function(query){
+	request("https://api.projectoxford.ai/luis/v2.0/apps/1ca69cad-7743-4016-bcfd-aa7fea2dee15?subscription-key=a57d3db3f47c4eacab1c9df496b2535f&q=I%20am%20small&verbose=true", function(error, response, body){
+		var json = JSON.parse(body)
+		console.log([json.intents[0].intent,json.entities[0].entity])
+		return [json.intents[0].intent,json.entities[0].entity]
+	});
+}
 
+bot.dialog('/', [
+    function (session) {
+        builder.Prompts.text(session, "Hello, what can I help you with?");
+    },
+    function (session, results) {
+
+        session.userData.initial = luis(results.response);
+        builder.Prompts.text(session, "Okay, what's your price range? [x-y€]"); 
+    },
+    function (session, results) {
+        session.userData.price = results.response;
+        //luis -> intent, entity
+        builder.Prompts.text(session, "And what size are you? [s, m, l]"); 
+    },/*
+    function (session, results) {
+        session.userData.size = results.respons;
+        //luis -> intent, entity
+        builder.Prompts.number(session, "Do you want me to show only sales?"); 
+    },
+    function (session, results) {
+        session.userData.sale = results.response;
+        //luis -> intent, entity
+        builder.Prompts.number(session, "How old are you?"); 
+    },
+    function (session, results) {
+        session.userData.ageGroup = results.response;
+        //luis -> intent, entity
+        builder.Prompts.number(session, "Are you looking for a specific brand?"); 
+    },
+    function (session, results) {
+        session.userData.name = results.response;
+        //luis -> intent, entity
+        builder.Prompts.number(session, "Okay, what's your price range?"); 
+    },
+    function (session, results) {
+        session.userData.name = results.response;
+        //luis -> intent, entity
+        builder.Prompts.number(session, "Okay, what's your price range?"); 
+    },
+    function (session, results) {
+        session.userData.name = results.response;
+        //luis -> intent, entity
+        builder.Prompts.number(session, "Okay, what's your price range?"); 
+    },*/
+
+
+    function (session, results) {
+        session.userData.size = results.response;
+        console.log(session.userData.price, session.userData.size)
+        var url = 'https://api.zalando.com/articles?price='+session.userData.price+'&size='+session.userData.size//+"&"+session.userData.initial[0]+'='+session.userData.initial[1]
+        request(url, function (error, response, body) {
+			if (!error && response.statusCode == 200) {
+			   var cats = JSON.parse(body)
+			   var cards =[];
+			   for (var i = cats.content.length - 1; i >= 0; i--) {
+			   	cards.push(cats["content"][i].name)
+			   }
+			   
+			   builder.Prompts.choice(session, "Does any of the fit the bill?", cards);
+			     }});
+        
+    },
+    function (session, results) {
+        session.userData.language = results.response.entity;
+        session.send("Got it... " + session.userData.language + ".");
+    }
+]);
 // Handle message from user
-bot.dialog('/', function (session) {
+bot.dialog('/i', function (session) {
     var queuedMessage = { address: session.message.address, text: session.message.text };
     // add message to queue
     session.sendTyping();
@@ -47,7 +121,7 @@ bot.dialog('/', function (session) {
                 
                     session.send("You can pass a custom message to Prompts.choice() that will present the user with a carousel of cards to select from. Each card can even support multiple actions.");
         
-        // Ask the user to select an item from a carousel.
+        /* Ask the user to select an item from a carousel.
         request('https://api.zalando.com/categories', function (error, response, body) {
             if (!error && response.statusCode == 200) {
                 cats = JSON.parse(body);
@@ -59,7 +133,8 @@ bot.dialog('/', function (session) {
                                 .attachmentLayout(builder.AttachmentLayout.carousel)
                                 .attachments(cards);
                             builder.Prompts.choice(session, msg, "select:100|select:101|select:102");
-                }}});
+                }}});*/
+                    
                     
             
 
